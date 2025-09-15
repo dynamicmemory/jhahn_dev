0508585527397cc83b4a0d618392c4ccaec89e9c
dynamicmemory <joshua.hahn@live.com>
2025-09-15 16:14:02 +1000

    Basic rebuild complete, no styles applied yet

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="/css/reset.css">
         <link rel="stylesheet" type="text/css" href="/css/main.css">
+        <link rel="stylesheet" type="text/css" href="/css/thoughts.css">
         <script src="js/main.js"></script>
     </head>
     <body>
@@ -14,12 +15,12 @@
             <nav>
                 <div id="left-nav">
                     <h1 class="nav-text">
-                        <a href="/index.php">MEMORY VOID</a>
+                        <a href="/index.php">{MEMORY VOID}</a>
                     </h1>
                 </div>
                 <div id="right-nav">
                     <h1 class="nav-text">
-                        <a href="/thoughts.php">Thoughts</a> 
+                        <a href="/thoughts.php">(&Thoughts)</a> 
                     </h1>
                 </div>
             </nav>
--- thoughts.php
+++ thoughts.php
@@ -1,14 +1,69 @@
 <?php 
-// Add php code for class stuff 
+// Get the json data 
+$database = file_get_contents("articles/database.json");
+// Reverse the db so newest is first
+$articles = array_reverse(json_decode($database, true));
+
+// Check if/which article was clicked and set it to a varaible to expose it in html
+$clickedArticle = null;
+if (isset($_GET['post'])) {
+    $articlePath = $_GET['post'];
+
+    // Protects against someone injecting an address to a folder we don't want to show
+    if (strpos($articlePath, "articles/") === 0 && file_exists($articlePath)) {
+        $content = file_get_contents($articlePath);
+
+        foreach ($articles as $article) {
+            if ($article['content'] === $articlePath) {
+                $clickedArticle = $article;
+                break;
+            }
+        }
+    }
+    else 
+        $content = "Invalid Article.";
+}
+// Load the latest article if none is selected (on page load)
+else {
+    if (!empty($articles)) {
+        $clickedArticle = $articles[0];
+        $content = file_get_contents($clickedArticle['content']);
+    }
+}
 ?>
+
 <?php include "header.php" ?>
+<div id="thoughts-container">
+    <div id="left-col">
+        <ul>
+        <!--For every article in the db, loop through and display the name and date-->
+        <?php  
+            if ($articles) {
+                foreach($articles as $article) {
+                    echo "<li><a href='?post=" . urlencode($article['content']) . 
+                        "'>" . htmlspecialchars($article['title']) . "</a></li>";
+                    echo "<small>" . htmlspecialchars($article['date']) . "</small>";
+                    echo "<br><hr>";
+                }
+            }
+            else {
+                echo "<p>No thoughts were found</p>";
+            }
+        ?>
+        </ul>
+    </div>
 
-<div id="main-container">
-    <h1>These are my thoughts</h1>
-
-    <p id="timer">0h: 0m: 0s</p>
-    <button id="start_button">Start</button>
-    <button id="reset_button">Reset</button>
+    <div id="center-col">
+        <div id="thoughts-content">
+            <h1 id="thoughts-title">
+            <?php echo htmlspecialchars($clickedArticle['title']);?>
+            </h1>
+            <p id="thoughts-date">Published - 
+            <?php echo htmlspecialchars($clickedArticle['date']);?>
+            </p>
+            <p id="thoughts-body"><?php echo $content;?>
+            </p>
+        </div>
+    </div>
 </div>
-
 <?php include "footer.php" ?>
