1ada58580cefe4faf0fd00ed9ac5bcc3d7a2124e
dynamicmemory <joshua.hahn@live.com>
2025-12-01 14:29:43 +1000

    Added ability to delete projects from backend

--- admin/projects.php
+++ admin/projects.php
@@ -10,27 +10,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Code for adding a new project
     if ($_POST["mode"] === "create") {
-    $statement = $db->prepare(
+        $statement = $db->prepare(
             "INSERT OR IGNORE INTO projects (name, slug, description) VALUES (?, ?, ?)");
-    $statement->bindValue(1, $name, SQLITE3_TEXT);
-    $statement->bindValue(2, $slug, SQLITE3_TEXT);
-    $statement->bindValue(3, $description, SQLITE3_TEXT);
-    $statement->execute();
+        $statement->bindValue(1, $name, SQLITE3_TEXT);
+        $statement->bindValue(2, $slug, SQLITE3_TEXT);
+        $statement->bindValue(3, $description, SQLITE3_TEXT);
+        $statement->execute();
 
-    echo "<p>Created!</p>";
+        echo "<p>Created!</p>";
     }
 
     // Code for updating a project 
     elseif ($_POST["mode"] === "update") {
-    $id = $_POST['id'];
-    $statement = $db->prepare(
-        "UPDATE projects SET name = ?, slug = ?, description = ? WHERE id = ?");
-    $statement->bindValue(1, $name, SQLITE3_TEXT);
-    $statement->bindValue(2, $slug, SQLITE3_TEXT);
-    $statement->bindValue(3, $description, SQLITE3_TEXT);
-    $statement->bindValue(4, $id, SQLITE3_INTEGER);
-    $statement->execute();
-    echo "<p>Updated!</p>";
+        $id = $_POST['id'];
+        $statement = $db->prepare(
+            "UPDATE projects SET name = ?, slug = ?, description = ? WHERE id = ?");
+        $statement->bindValue(1, $name, SQLITE3_TEXT);
+        $statement->bindValue(2, $slug, SQLITE3_TEXT);
+        $statement->bindValue(3, $description, SQLITE3_TEXT);
+        $statement->bindValue(4, $id, SQLITE3_INTEGER);
+        $statement->execute();
+        echo "<p>Updated!</p>";
     }
 }
 
@@ -39,6 +39,15 @@ $isNew = false;
 
 if (isset($_GET["action"]) && $_GET["action"] === "new") {
     $isNew = true;
+}
+
+if (isset($_GET["delete"])) {
+    $id = (int)$_GET["delete"];
+
+    $statement = $db->prepare("DELETE FROM projects WHERE id = ?");
+    $statement->bindValue(1, $id, SQLITE3_INTEGER);
+    $statement->execute();
+    echo "<p>Deleted project $id</p>";
 }
 
 if (isset($_GET['id'])) {
@@ -52,65 +61,89 @@ $projects = $db->query("SELECT id, name FROM projects ORDER BY id ASC");
 
 ?>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <h1>Projects DB editor</h1>
 <!-- Main container -->
 <div style="display: flex; gap: 2rem; max-width: 1000px;"> 
 
-  <!-- Left col -->
-  <div style="min-width: 200px;"> 
-    <h3>Projects</h3>
-    <a href="?action=new">Add New Project</a>
-    <ul> 
-    <?php while ($p = $projects->fetchArray(SQLITE3_ASSOC)) : ?>
-      <li>
-        <a href="?id=<?= $p["id"] ?>"> 
-          <?= htmlspecialchars($p["name"]) ?> 
-        </a>
-      </li>
-    <?php endwhile ?> 
-    </ul>
-  </div>
+    <!-- Left col -->
+    <div style="min-width: 200px;"> 
+        <h3>Projects</h3>
+        <a href="?action=new">Add New Project</a>
+        <ul> 
+            <?php while ($p = $projects->fetchArray(SQLITE3_ASSOC)) : ?>
+            <li>
+                <a href="?id=<?= $p["id"] ?>"> 
+                    <?= htmlspecialchars($p["name"]) ?> 
+                </a>
+            </li>
+            <?php endwhile ?> 
+        </ul>
+    </div>
 
-  <!-- Center col-->
-  <div style="flex: 1;">
-  <?php  if ($selected_project || $isNew): ?>
-    <?php if (!$isNew): ?>
-      <h3>Editing: <?= htmlspecialchars($selected_project["name"]) ?></h3> 
-    <?php else: ?>
-      <h3>Adding New Project</h3>
-    <?php endif; ?>
+    <!-- Center col-->
+    <div style="flex: 1;">
+        <?php  if ($selected_project || $isNew): ?>
+        <?php if (!$isNew): ?>
+        <h3>Editing: <?= htmlspecialchars($selected_project["name"]) ?></h3> 
+        <?php else: ?>
+        <h3>Adding New Project</h3>
+        <?php endif; ?>
 
-    <form method="POST"> 
-      <fieldset> 
-        <input type="hidden" name="mode" value="<?= $isNew ? "create" : "update" ?>">
-      <?php if (!$isNew): ?>
-        <input type="hidden" name="id" value="<?= $selected_project["id"]?>">
-      <?php endif; ?>
+        <form method="POST"> 
+            <fieldset> 
+                <input type="hidden" name="mode" value="<?= $isNew ? "create" : "update" ?>">
+                <?php if (!$isNew): ?>
+                <input type="hidden" name="id" value="<?= $selected_project["id"]?>">
+                <?php endif; ?>
 
-        <label>Name: </label><br>
-        <input name="name" value="<?= htmlspecialchars($selected_project["name"]) ?>"
-               style="width: 100%"><br><br>
+                <label>Name: </label><br>
+                <input name="name" value="<?= htmlspecialchars($selected_project["name"]) ?>"
+                    style="width: 100%"><br><br>
 
-        <label>URL-Slug: </label><br>
-        <input name="slug" value="<?= htmlspecialchars($selected_project["slug"]) ?>"
-               style="width: 100%"><br><br>
+                <label>URL-Slug: </label><br>
+                <input name="slug" value="<?= htmlspecialchars($selected_project["slug"]) ?>"
+                    style="width: 100%"><br><br>
 
-        <label>Content (Markdown body): </label><br>
-        <textarea name="description" rows="30" style="width: 100%;"><?= htmlspecialchars($selected_project["description"]) ?>
-        </textarea><br><br>
+                <label>Content (Markdown body): </label><br>
+                <textarea name="description" rows="30" style="width: 100%;"><?= htmlspecialchars($selected_project["description"]) ?></textarea>
+                <br><br>
 
-        <!-- ADD JSON MEDIA OBJECTS IF I CHOOSE TO IMPLEMENT HERE-->
-        <button type="submit">
-          <?= $isNew ? "Add Project" : "Save Changes" ?>
-        </button>
+                <!-- ADD JSON MEDIA OBJECTS IF I CHOOSE TO IMPLEMENT HERE-->
+                <button type="submit">
+                    <?= $isNew ? "Add Project" : "Save Changes" ?>
+                </button>
 
-      </fieldset> 
+                <?php if (!$isNew): ?>
+                <p style="margin-top:1rem; float: right;">
+                    <button> 
+                        <a href="?delete=<?= $selected_project["id"] ?>"
+                            onclick="return confirm('Are you sure you want to delete this project');"
+                            style="color:red; text-decoration: none;"> 
+                            Delete this project
+                        </a>
+                    </button>
+                </p>
+                <?php endif; ?>
 
+            </fieldset> 
+        </form>
 
-    </form>
-
-  <?php else: ?>
-    <p>Select a project from the left to edit.</>
-  <?php endif; ?>
-  </div>
+        <?php else: ?>
+        <p>Select a project from the left to edit.</>
+        <?php endif; ?>
+    </div>
 </div>
