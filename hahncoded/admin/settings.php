2dfd39a22815f76a4824d2953b4cdd9b3bea1038
dynamicmemory <joshua.hahn@live.com>
2025-12-11 14:21:17 +1000

    Continued working on settings page

--- hahncoded/admin/settings.php
+++ hahncoded/admin/settings.php
@@ -23,18 +23,36 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 
     // Update an exisiting setting 
-    if ($_POST["mode"] === "update") {
-        /* $id = $_ */
-
-
+    elseif ($_POST["mode"] === "update") {
+        $id = $_POST["id"];
+        $statement = $database->prepare("UPDATE settings 
+            SET key = ?, value = ?, type = ?, tag = ? WHERE id = ?");
+        $statement->bindValue(1, $key, SQLITE3_TEXT);
+        $statement->bindValue(2, $value, SQLITE3_TEXT);
+        $statement->bindValue(3, $type, SQLITE3_TEXT);
+        $statement->bindValue(4, $tag, SQLITE3_TEXT);
+        $statement->bindValue(5, $id, SQLITE3_INTEGER);
+        $statement->execute();
         echo "<p>Successfully updated $key</p>";
     }
 }
 
-// All the get stuff and populating the fields
-// TODO: Get id from db, then i can do update above, then i can add and update
-//       settings, then i can build the function to serve up the data, then i 
-//       can place a call in home to load up the <p> to test it out.
+$selected_setting = null;
+$isNew = false;
+
+if (isset($_GET["action"]) && $_GET["action"] === "new") {
+    $isNew = true;
+}
+
+if (isset($_GET["id"])) {
+    $statement = $database->prepare("SELECT * FROM settings WHERE id = ?");
+    $statement->bindValue(1, $_GET["id"], SQLITE3_INTEGER);
+    $statement->execute();
+    $selected_setting = $statement->fetch(PDO::FETCH_ASSOC);
+}
+
+// Gets all the settings to display in the sidebar
+$settings = $database->query("SELECT id, key FROM settings ORDER BY id ASC");
 
 ?>
 <!-- Make a new header for the admin panel so i can load css scripts and the like-->
@@ -43,38 +61,50 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         <link rel="stylesheet" type="text/css" href="/css/admin.css">
     </head>
 
+    <h1>Settings</h1> 
+    <a href="index.php">Dashboard</a>
 
+    <!-- Main container-->
+    <div class="main-container">
+        <a href="?action=new">Add new setting</a>
 
-<h1>Settings</h1> 
-<a href="index.php">Dashboard</a>
+        <!-- Side panel-->
+        <div class="side-panel">
+            <ul> 
+                <?php while($s = $settings->fetch(PDO::FETCH_ASSOC)): ?>
+                <li>
+                    <a href="?id=<?=$s["id"]?>">
+                        <?=htmlspecialchars($s["key"])?> 
+                    </a>   
+                </li>
+                <?php endwhile ?>
+            </ul>
 
-<!-- Main container-->
-  <div class="main-container">
+        </div>
+        <!-- Center panel-->
+        <div class="center-panel">
 
-<!-- Side panel-->
-    <div class="side-panel">
+            <form method="POST">
+                <fieldset> 
+                    <input type="hidden" name="mode" value="<?=$isNew ? 'create' : 'update'?>">
+                    <?php if (!$isNew): ?>
+                    <input type="hidden" name="id" value="<?=$selected_setting["id"]?>">
+                    <?php endif; ?>
+                    <label>Key</label>
+                    <input name="key" value="<?= htmlspecialchars($selected_setting["key"])?>">
+                    <label>Value</label>
+                    <textarea name="value" value="<?=htmlspecialchars($selected_setting["value"])?>">
+                    </textarea>
+                    <label>Type</label>
+                    <input name="type" value="<?=htmlspecialchars($selected_setting["type"])?>">
+                    <label>Tag</label>
+                    <input name="tag" value="<?=htmlspecialchars($selected_setting["tag"])?>">
+                    <button type="submit">SUMBIT</button>
 
+                </fieldset>
+            </form>
+
+        </div>
     </div>
-<!-- Center panel-->
-    <div class="center-panel">
-
-      <form method="POST">
-        <fieldset> 
-          <label>Key</label>
-          <input name="key"> </input>
-          <label>Value</label>
-          <textarea name="value"> </textarea>
-          <label>Type</label>
-          <input name="type"> </input>
-          <label>Tag</label>
-          <input name="tag"> </input>
-          <button type="submit">SUMBIT</button>
-
-
-        </fieldset>
-      </form>
-
-    </div>
-  </div>
 </html>
 <!-- -->
